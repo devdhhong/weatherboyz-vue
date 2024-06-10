4b94201b1045c299077561808f6d451b0718e866
Dahye Hong <happyhong28@naver.com>
2024-06-11 00:29:57 +0900

    0611 라디오버튼 CSS 추가

--- weatherboyz/src/components/SettingView.vue
+++ weatherboyz/src/components/SettingView.vue
@@ -1,56 +1,64 @@
 <template>
   <div id="SettingView">
     <li class="settingItem" @click="setNation">
-      <div class="title">언어 설정</div>  
-      <div class=""><input type="radio" name="language">KOR</div>
-      <div class=""><input type="radio" name="language">ENG</div>
+      <div class="title">언어 설정</div>
+      <ul class="optionList">
+        <li>
+          <input id="r1" type="radio" name="language" value="KOR" checked/>
+          <label for="r1">한국어</label>
+        </li>
+        <li>
+          <input id="r1" type="radio" name="language" value="ENG" />
+          <label for="r1">영어</label>
+        </li>
+      </ul>
+      <!-- <div class=""><input type="radio" name="language" />KOR</div> -->
+      <!-- <div class=""><input type="radio" name="language" />ENG</div> -->
     </li>
     <li class="settingItem" @click="setTheme">
-      <div class="title">멤버 설정</div>  
-        <div class="selectBtn" @click="toggleActive">
-            <span class="sBtn-text">단체</span>
-            <!-- <i class="bx bx-chevron-down"></i> -->
-        </div>
-        <ul class="optionList" :class="{ active: isActive }">
-            <li class="option" @click="toggleActive">
-                <i class="bx bxl-github" style="color: #171515;"></i>
-                <span class="option-text">Github</span>
-            </li>
-            <li class="option">
-                <i class="bx bxl-instagram-alt" style="color: #E1306C;"></i>
-                <span class="option-text">Instagram</span>
-            </li>
-            <li class="option">
-                <i class="bx bxl-linkedin-square" style="color: #0E76A8;"></i>
-                <span class="option-text">Linkedin</span>
-            </li>
-            <li class="option">
-                <i class="bx bxl-facebook-circle" style="color: #4267B2;"></i>
-                <span class="option-text">Facebook</span>
-            </li>
-            <li class="option">
-                <i class="bx bxl-twitter" style="color: #1DA1F2;"></i>
-                <span class="option-text">Twitter</span>
-            </li>
-        </ul>
+      <div class="title">테마 설정</div>
+      <button class="btn-select" @click="toggleActive">단체</button>
+       <ul class="list-member selectBox" :class="{ active: isActive }">
+          <li><button type="button" @click="toggleActive">Python</button></li>
+          <li><button type="button" @click="toggleActive">Java</button></li>
+          <li><button type="button">JavaScript</button></li>
+          <li><button type="button">C#</button></li>
+          <li><button type="button">C/C++</button></li>
+          <li><button type="button">C/C++</button></li>
+          <li><button type="button">C/C++</button></li>
+          <li><button type="button">C/C++</button></li>
+          <li><button type="button">C/C++</button></li>
+          <li><button type="button">C/C++</button></li>
+      </ul>
     </li>
     <li class="settingItem" @click="setNation">
-      <div class="title">테마 설정</div>  
-      <div class=""><input type="radio" name="theme">밝은 모드</div>
-      <div class=""><input type="radio" name="theme">어두운 모드</div>
+      <div class="title">화면 설정</div>
+      <ul class="optionList">
+        <li>
+          <input id="r1" type="radio" name="display" value="LIGHT" checked/>
+          <label for="r1">라이트</label>
+        </li>
+        <li>
+          <input id="r1" type="radio" name="display" value="DARK" />
+          <label for="r1">다크</label>
+        </li>
+      </ul>
     </li>
     <li class="settingItem" @click="setNation">
-      <div class="title">기타</div>  
-      <div class="">이용방법</div>
+      <div class="title">기타</div>
+      <ul class="optionList">
+        <li>
+          <div class="">이용방법</div>
+        </li>
+      </ul>
     </li>
     <li class="copyRight">
-     <p>© 2024 dhhong. All rights reserved.</p>
+      <p>© 2024 dhhong. All rights reserved.</p>
     </li>
   </div>
 </template>
 
 <script>
- 
 export default {
   data() {
     return {
@@ -58,18 +66,17 @@ export default {
     };
   },
   methods: {
-    setNation: function(){
+    setNation: function () {
       // alert("1");
     },
-    setTheme: function(){
+    setTheme: function () {
       // alert("2");
     },
-    toggleActive: function(){
+    toggleActive: function () {
       this.isActive = !this.isActive;
-    }
-  }
-} 
-
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -80,10 +87,11 @@ export default {
   position: fixed;
   top: 10vh;
 
-  .settingItem, .copyRight {
+  .settingItem,
+  .copyRight {
     padding: 5% 7%;
   }
-  
+
   .settingItem {
     .title {
       width: 100%;
@@ -94,26 +102,35 @@ export default {
       padding-left: 4%; /* 세로 중앙 정렬 */
       font-size: 1.1rem;
       font-weight: 600;
+      border-radius: 5px;
     }
-    
-    select {
-      width: 100%;
-      // font-size: 1rem;
-      color: #60666d;
 
-      option {
-      }
+    ul {
+
     }
+
+    li {
+      margin : 16px;
+    }
+
   }
 
-  .optionList.active {
+  .selectBox.active {
+    width: 80%;
+    max-height: 40%; /* 최대 높이를 설정 */
+    overflow-y: auto;  /* 수직 스크롤이 생기도록 설정 */
+    // position: fixed;
+    // // left: 5%;
+    // // top: 30vh;
+    background: #ffffff;
     display: block;
   }
 
-  .optionList {
+  .selectBox {
     display: none;
   }
 
+  //저작권 표기
   .copyRight {
     position: fixed;
     bottom: 0;

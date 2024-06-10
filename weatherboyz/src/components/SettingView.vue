8a97df0497fac66b03e59ae481c191b5917759f8
dhhong <devdhhong@gmail.com>
2024-06-10 17:45:24 +0900

    0610 임시

--- weatherboyz/src/components/SettingView.vue
+++ weatherboyz/src/components/SettingView.vue
@@ -7,22 +7,41 @@
     </li>
     <li class="settingItem" @click="setTheme">
       <div class="title">멤버 설정</div>  
-      <div class=""><input type="radio" name="member">상연</div>
-      <div class=""><input type="radio" name="member">제이콥</div>
-      <div class=""><input type="radio" name="member">영훈</div>
-      <div class=""><input type="radio" name="member">현재</div>
-      <div class=""><input type="radio" name="member">주연</div>
-      <div class=""><input type="radio" name="member">케빈</div>
-      <div class=""><input type="radio" name="member">뉴</div>
-      <div class=""><input type="radio" name="member">큐</div>
-      <div class=""><input type="radio" name="member">학년</div>
-      <div class=""><input type="radio" name="member">선우</div>
-      <div class=""><input type="radio" name="member">에릭</div>
+        <div class="selectBtn" @click="toggleActive">
+            <span class="sBtn-text">단체</span>
+            <!-- <i class="bx bx-chevron-down"></i> -->
+        </div>
+        <ul class="optionList" :class="{ active: isActive }">
+            <li class="option" @click="toggleActive">
+                <i class="bx bxl-github" style="color: #171515;"></i>
+                <span class="option-text">Github</span>
+            </li>
+            <li class="option">
+                <i class="bx bxl-instagram-alt" style="color: #E1306C;"></i>
+                <span class="option-text">Instagram</span>
+            </li>
+            <li class="option">
+                <i class="bx bxl-linkedin-square" style="color: #0E76A8;"></i>
+                <span class="option-text">Linkedin</span>
+            </li>
+            <li class="option">
+                <i class="bx bxl-facebook-circle" style="color: #4267B2;"></i>
+                <span class="option-text">Facebook</span>
+            </li>
+            <li class="option">
+                <i class="bx bxl-twitter" style="color: #1DA1F2;"></i>
+                <span class="option-text">Twitter</span>
+            </li>
+        </ul>
     </li>
     <li class="settingItem" @click="setNation">
       <div class="title">테마 설정</div>  
       <div class=""><input type="radio" name="theme">밝은 모드</div>
       <div class=""><input type="radio" name="theme">어두운 모드</div>
+    </li>
+    <li class="settingItem" @click="setNation">
+      <div class="title">기타</div>  
+      <div class="">이용방법</div>
     </li>
     <li class="copyRight">
      <p>© 2024 dhhong. All rights reserved.</p>
@@ -33,6 +52,11 @@
 <script>
  
 export default {
+  data() {
+    return {
+      isActive: false,
+    };
+  },
   methods: {
     setNation: function(){
       // alert("1");
@@ -40,6 +64,9 @@ export default {
     setTheme: function(){
       // alert("2");
     },
+    toggleActive: function(){
+      this.isActive = !this.isActive;
+    }
   }
 } 
 
@@ -53,50 +80,51 @@ export default {
   position: fixed;
   top: 10vh;
 
-  li {
+  .settingItem, .copyRight {
     padding: 5% 7%;
-
   }
-
+  
   .settingItem {
-    
     .title {
       width: 100%;
       height: 40px;
       background-color: #c0deff79;
-
       display: flex;
-      align-items: center; /* 세로 중앙 정렬 */
-
-      padding-left: 4%;
+      align-items: center;
+      padding-left: 4%; /* 세로 중앙 정렬 */
       font-size: 1.1rem;
       font-weight: 600;
     }
+    
+    select {
+      width: 100%;
+      // font-size: 1rem;
+      color: #60666d;
 
-    input {
-      font-size: 30px;
-      font-weight: 600;
+      option {
+      }
     }
   }
 
+  .optionList.active {
+    display: block;
+  }
+
+  .optionList {
+    display: none;
+  }
+
   .copyRight {
-    // display: flex;
-    // align-items: center; /* 세로 중앙 정렬 */
     position: fixed;
     bottom: 0;
     right: 0;
-    
+
     p {
-      // font-size: 0.5rem;
       font-size: 10px;
       font-weight: 300;
       color: green($color: #000000);
     }
   }
-
-  
 }
-
-
 
 </style>
